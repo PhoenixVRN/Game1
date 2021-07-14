9ff5d4e3bd03dc00b7b5446da657c91dfe45823a
Evgen <ekspert-ocenka@mail.ru>
2021-07-14 12:30:10 +0300

    0.20

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -6,6 +6,8 @@ public class Enemy : MonoBehaviour
 {
     public int health = 10;
     public GameObject dyrka;
+    public LayerMask layerMask;
+    public GameObject Eay;
 
     public float speedRotation = 30;
     public float distanceVisual;
@@ -17,18 +19,32 @@ public class Enemy : MonoBehaviour
     }
     void FixedUpdate()
     {
-        var direction = _player.position - transform.position;
-                var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
- 
+//        var direction = _player.position - transform.position;
+//       var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+//        Eay.transform.rotation = Quaternion.Euler(0, 0, angle);
 
-        if (Vector2.Distance(transform.position, _player.position) < distanceVisual)
-        {
-            var a = transform.rotation;
-            var b = Quaternion.Euler(0, 0, angle);
-           var s = Quaternion.Angle(a, b);
 
-            transform.rotation= Quaternion.Slerp(a, b, (Time.deltaTime * speedRotation)/s);
-        }
+//        Ray ray = new Ray(Eay.transform.position, Eay.transform.right);
+//        Debug.DrawRay(Eay.transform.position, Eay.transform.right * distanceVisual, Color.red);
+
+//                RaycastHit hit;
+//        if (Physics.Raycast(ray, out hit))
+ //       {
+//            Debug.Log(hit.rigidbody);
+            var direction = _player.position - transform.position;
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+
+            if (Vector2.Distance(transform.position, _player.position) < distanceVisual)
+            {
+                var a = transform.rotation;
+                var b = Quaternion.Euler(0, 0, angle);
+                var s = Quaternion.Angle(a, b);
+
+                transform.rotation = Quaternion.Slerp(a, b, (Time.deltaTime * speedRotation) / s);
+            }
+//        }
+    
     }
 
     public void TakeDamage (int damage)
